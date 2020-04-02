a48295f8532eefdc536b8138ae7e096a601590b1
Jake Kwon <jakej.kwon@gmail.com>
2020-04-01 22:14:30 -0400

    Add Contact contents

--- src/sections/contact/contact.components.jsx
+++ src/sections/contact/contact.components.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
 import {
-    ContactContainer
+    ContactContainer,
+    ContactHeader,
+    ContactSubHeader,
+    ContentsContainer 
 } from './contact.styles'
 
 const Contact = () => {
     return (
-        <ContactContainer id="about">
-            Contact
+        <ContactContainer>
+            <ContactHeader>What's Next?</ContactHeader>
+            <ContactSubHeader>Get In Touch</ContactSubHeader>
+            <ContentsContainer>I'm not looking for freelance opportunities, my inbox is always open. Whether for a potential project or just to say hi, I'll try my best to answer your email!</ContentsContainer>
         </ContactContainer>
     )
 }
--- src/sections/contact/contact.styles.jsx
+++ src/sections/contact/contact.styles.jsx
@@ -2,3 +2,12 @@ import styled from 'styled-components'
 
 export const ContactContainer = styled.div`
 `
+
+export const ContactHeader = styled.div`
+`
+
+export const ContactSubHeader = styled.div`
+`
+
+export const ContentsContainer = styled.div`
+`
